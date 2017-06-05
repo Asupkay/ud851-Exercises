bea259e9187ff57d646cfc28bc64d4e88c636446
Asupkay <asupkay1124@gmail.com>
2017-06-05 12:37:40 -0400

    Fix Lesson 9 Exercise 6

--- Lesson09-ToDo-List/T09.06-Exercise-Delete/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ Lesson09-ToDo-List/T09.06-Exercise-Delete/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -173,7 +173,9 @@ public class TaskContentProvider extends ContentProvider {
         }
 
         // COMPLETED (3) Notify the resolver of a change and return the number of items deleted
-        getContext().getContentResolver().notifyChange(uri, null);
+        if(nDeleted != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
         return nDeleted;
     }
 
