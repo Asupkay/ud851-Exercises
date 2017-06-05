8f23f42a339512e9f4309e3537b71aef5acc913c
Asupkay <asupkay1124@gmail.com>
2017-06-05 12:09:45 -0400

    Complete Lesson 9 Exercise 5

--- Lesson09-ToDo-List/T09.05-Exercise-QueryAllTasks/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ Lesson09-ToDo-List/T09.05-Exercise-QueryAllTasks/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -120,15 +120,29 @@ public class TaskContentProvider extends ContentProvider {
     public Cursor query(@NonNull Uri uri, String[] projection, String selection,
                         String[] selectionArgs, String sortOrder) {
 
-        // TODO (1) Get access to underlying database (read-only for query)
+        // COMPLETED (1) Get access to underlying database (read-only for query)
+        SQLiteDatabase mDB = mTaskDbHelper.getReadableDatabase();
+        // COMPLETED (2) Write URI match code and set a variable to return a Cursor
+        int match = sUriMatcher.match(uri);
 
-        // TODO (2) Write URI match code and set a variable to return a Cursor
-
-        // TODO (3) Query for the tasks directory and write a default case
-
-        // TODO (4) Set a notification URI on the Cursor and return that Cursor
-
-        throw new UnsupportedOperationException("Not yet implemented");
+        Cursor mCursor;
+        // COMPLETED (3) Query for the tasks directory and write a default case
+        switch (match) {
+            case (TASKS):
+                mCursor = mDB.query(TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown Uri " + uri);
+        }
+        // COMPLETED (4) Set a notification URI on the Cursor and return that Cursor
+        mCursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return mCursor;
     }
 
 
