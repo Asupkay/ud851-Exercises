8dea6d993b0f1e8ce1d21b3cc300eff436044d94
Asupkay <asupkay1124@gmail.com>
2017-06-06 11:37:51 -0400

    Complete Lesson 10 Exercise 1

--- Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/ReminderTasks.java
+++ Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/ReminderTasks.java
@@ -1,7 +1,23 @@
 package com.example.android.background.sync;
 
-// TODO (1) Create a class called ReminderTasks
+import android.content.Context;
 
+import com.example.android.background.utilities.PreferenceUtilities;
+
+// TODO (1) Create a class called ReminderTasks
+public class ReminderTasks {
+    public static final String ACTION_INCREMENT_WATER_COUNT = "ACTION_INCREMENT_WATER_COUNT";
+
+    public static void executeTask(Context context, String action){
+        if(action.matches(ACTION_INCREMENT_WATER_COUNT)){
+            incrementWaterCount(context);
+        }
+    }
+
+    private static void incrementWaterCount(Context context){
+        PreferenceUtilities.incrementWaterCount(context);
+    }
+}
 // TODO (2) Create a public static constant String called ACTION_INCREMENT_WATER_COUNT
 
 // TODO (6) Create a public static void method called executeTask
@@ -10,5 +26,6 @@ package com.example.android.background.sync;
 // TODO (8) If the action equals ACTION_INCREMENT_WATER_COUNT, call this class's incrementWaterCount
 
 // TODO (3) Create a private static void method called incrementWaterCount
+
 // TODO (4) Add a Context called context to the argument list
 // TODO (5) From incrementWaterCount, call the PreferenceUtility method that will ultimately update the water count
